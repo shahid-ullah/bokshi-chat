468314894f7d0e9d4656409238fe573f6173e153
shahid <shahid.ice.nstu@gmail.com>
2021-05-16 14:58:23 +0600

    update remove user api

--- core/api.py
+++ core/api.py
@@ -7,6 +7,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import generics, mixins, parsers, status, viewsets
 from rest_framework.authentication import (SessionAuthentication,
                                            TokenAuthentication)
+from rest_framework.decorators import api_view, authentication_classes
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -17,13 +18,13 @@ from core.models import ChatGroup, ChatGroupMessage, MessageModel, Relationship
 from core.serializers import (ChatGroupDetailSerializer,
                               ChatGroupMessageDetailSerializer,
                               ChatGroupMessageSerializer, ChatGroupSerializer,
-                              MessageModelSerializer,
-                              RelationshipModelSerializer, UserModelSerializer,FileSerializer,RemoveUserSerializer)
+                              FileSerializer, MessageModelSerializer,
+                              RelationshipModelSerializer,
+                              RemoveUserSerializer, UserModelSerializer)
 
-from django.contrib.auth import get_user_model
-from rest_framework.views import APIView
-from rest_framework.decorators import api_view,authentication_classes
 User = get_user_model()
+
+
 class CsrfExemptSessionAuthentication(SessionAuthentication):
     """
     SessionAuthentication scheme used by DRF. DRF's SessionAuthentication uses
@@ -46,7 +47,7 @@ class MessagePagination(PageNumberPagination):
 class MessageModelViewSet(ModelViewSet):
     queryset = MessageModel.objects.all()
     serializer_class = MessageModelSerializer
-    parser_classes=[parsers.FormParser,parsers.MultiPartParser]
+    parser_classes = [parsers.FormParser, parsers.MultiPartParser]
     allowed_methods = ('GET', 'POST', 'HEAD', 'OPTIONS')
     authentication_classes = (
         CsrfExemptSessionAuthentication,
@@ -246,14 +247,13 @@ class GetFilesAPI(mixins.ListModelMixin, generics.GenericAPIView):
     queryset = MessageModel.objects.values_list('files')
     queryset = MessageModel.objects.all()
     serializer_class = FileSerializer
-    parser_classes=[parsers.FormParser,parsers.MultiPartParser]
-    allowed_methods = ('GET')
+    parser_classes = [parsers.FormParser, parsers.MultiPartParser]
+    allowed_methods = 'GET'
     authentication_classes = (
         CsrfExemptSessionAuthentication,
         TokenAuthentication,
     )
     # authentication_classes = (CsrfExemptSessionAuthentication,)
-
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
@@ -282,23 +282,31 @@ class GetFilesAPI(mixins.ListModelMixin, generics.GenericAPIView):
 #     )
 #     lookup_field = "creator_id"
 
+
 @api_view(['POST'])
-@authentication_classes([ CsrfExemptSessionAuthentication,TokenAuthentication,])
+@authentication_classes(
+    [
+        CsrfExemptSessionAuthentication,
+        TokenAuthentication,
+    ]
+)
 def UserRemoveAPIView(request):
     if request.method == 'POST':
         creator = request.POST['creator']
         friend = request.POST['friend']
-        if(creator is not None and friend is not None):
+        if creator is not None and friend is not None:
             creatorObject = User.objects.get(username=creator)
-            friendObject = User.objects.get(username = friend)
-            Relationship.objects.filter(creator_id = creatorObject.id).filter(friends_id = friendObject.id).delete()
-            MessageModel.objects.filter(user_id = creatorObject.id).filter(recipient_id = friendObject.id).delete()
-            MessageModel.objects.filter(recipient_id = creatorObject.id).filter(user_id = friendObject.id).delete()
+            friendObject = User.objects.get(username=friend)
+            Relationship.objects.filter(creator_id=creatorObject.id).filter(
+                friend_id=friendObject.id
+            ).delete()
+            # MessageModel.objects.filter(user_id=creatorObject.id).filter(
+            #     recipient_id=friendObject.id
+            # ).delete()
+            # MessageModel.objects.filter(recipient_id=creatorObject.id).filter(
+            #     user_id=friendObject.id
+            # ).delete()
 
-
-            
-        
         return Response({})
-        
+
     return Response({})
-    
