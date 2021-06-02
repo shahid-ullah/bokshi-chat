75c92df956bb6569d9a41cba6d02adec8fd3110d
shahid <shahid.ice.nstu@gmail.com>
2021-06-02 12:36:40 +0600

    update home page TemplateView to function view

--- core/views.py
+++ core/views.py
@@ -1,5 +1,11 @@
 # core/views.py
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+
+
+@login_required
+def home(request):
+    return render(request, 'core/index.html')
 
 
 def group_view(request):
